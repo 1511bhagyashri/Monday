1d041fae595379e8bd37ef2667e09727491cec4f
kiran <kiran@LAPTOP-02L18SP5>
2022-07-26 16:32:09 +0530

    commit 2

---
+++ src/com/aspire/github/Sample2.java
@@ -0,0 +1,14 @@
+package com.aspire.github;
+
+public class Sample2 
+{
+	public static void main(String[] args)
+	{
+		int p=50;
+		int q=30;
+		int sub=p-q;
+		System.out.println("p-q="+sub);
+		
+	}
+
+}
